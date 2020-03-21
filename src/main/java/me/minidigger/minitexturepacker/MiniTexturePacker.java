17231bb5342723fd676be76b293b7565815c901b
MiniDigger <admin@minidigger.me>
2020-03-21 10:16:29 +0100

    allow brightening the images + allow running it

--- src/main/java/me/minidigger/minitexturepacker/MiniTexturePacker.java
+++ src/main/java/me/minidigger/minitexturepacker/MiniTexturePacker.java
@@ -1,5 +1,7 @@
 package me.minidigger.minitexturepacker;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
@@ -7,16 +9,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import javax.imageio.ImageIO;
 
 public class MiniTexturePacker {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java -jar minitexturepacker /path/to/dir");
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar minitexturepacker /path/to/dir <factor>");
             return;
         }
         Path mainDir = Path.of(args[0]);
-        new MiniTexturePacker(mainDir.resolve("original"), mainDir.resolve("patch"), mainDir.resolve("output")).patch();
+        float brightenFactor = Float.parseFloat(args[1]);
+
+        new MiniTexturePacker(mainDir.resolve("original"), mainDir.resolve("patch"), mainDir.resolve("output")).patch(brightenFactor);
     }
 
     private final Path original;
@@ -29,13 +34,15 @@ public class MiniTexturePacker {
         this.output = output;
     }
 
-    public void patch() {
+    public void patch(float brightenFactor) {
         cleanOutput();
         copyMeta();
-        copyFolder("textures");
-        copyFolder("sounds");
+        copyFolder("textures", brightenFactor);
+        copyFolder("sounds", 1);
         copySoundsJson();
-        copyFolder("optifine");
+        copyFolder("optifine", 1);
+        copyFolder("models", 1);
+        copyFolder("blockstates", 1);
         copySplashes();
         copyFont();
         // TODO blockstates and models
@@ -55,7 +62,7 @@ public class MiniTexturePacker {
     private void copyMeta() {
         System.out.print("Copying meta... ");
         for (String fileName : new String[]{"pack.mcmeta", "pack.png"}) {
-            copyFitting(fileName, original, patch, output);
+            copyFitting(fileName, original, patch, output, 1);
         }
         System.out.println("Done!");
     }
@@ -120,8 +127,11 @@ public class MiniTexturePacker {
 
     // util shit
 
-    private void copyFolder(String name) {
+    private void copyFolder(String name, float brightenFactor) {
         System.out.print("Creating " + name + " folder... ");
+        if (brightenFactor != 1) {
+            System.out.print("Also brighten it with factor " + brightenFactor + "... ");
+        }
         String folderName = "assets\\minecraft\\" + name;
         try {
             Files.createDirectories(output.resolve(folderName));
@@ -135,9 +145,9 @@ public class MiniTexturePacker {
             // copy over files from original, using patch files if they are there
             Files.list(original.resolve(folderName)).parallel().forEach(folder -> {
                 if (Files.isDirectory(folder)) {
-                    copyFolder(folderName, folder);
+                    copyFolder(folderName, folder, brightenFactor);
                 } else if (Files.isRegularFile(folder)) {
-                    copyFitting(folder.getFileName().toString(), original.resolve(folderName), patch.resolve(folderName), output.resolve(folderName));
+                    copyFitting(folder.getFileName().toString(), original.resolve(folderName), patch.resolve(folderName), output.resolve(folderName), brightenFactor);
                 }
             });
             // copy over fully custom files
@@ -153,7 +163,7 @@ public class MiniTexturePacker {
                             System.out.println("Error while creating " + outputFolder + " folder: " + e.getClass().getName() + ": " + e.getMessage());
                         }
                     }
-                    copyNewFiles(folder, outputFolder);
+                    copyNewFiles(folder, outputFolder, brightenFactor);
                 });
             }
             System.out.println("Done!");
@@ -162,7 +172,7 @@ public class MiniTexturePacker {
         }
     }
 
-    private void copyFolder(String folderPath, Path subFolder) {
+    private void copyFolder(String folderPath, Path subFolder, float brightenFactor) {
         // prepare output
         String subFolderName = subFolder.getFileName().toString();
         Path newOriginal = original.resolve(folderPath).resolve(subFolderName);
@@ -177,19 +187,19 @@ public class MiniTexturePacker {
         try {
             Files.list(newOriginal).parallel().forEach(file -> {
                 if (Files.isDirectory(file)) {
-                    copyFolder(folderPath + "\\" + subFolderName, file);
+                    copyFolder(folderPath + "\\" + subFolderName, file, brightenFactor);
                 } else if (Files.isRegularFile(file)) {
-                    copyFitting(file.getFileName().toString(), newOriginal, newPatch, newOutput);
+                    copyFitting(file.getFileName().toString(), newOriginal, newPatch, newOutput, brightenFactor);
                 }
             });
         } catch (IOException e) {
             System.out.println("Error while listing files in orig " + newOriginal + ": " + e.getClass().getName() + ": " + e.getMessage());
         }
         // then copy new files from patch
-        copyNewFiles(newPatch, newOutput);
+        copyNewFiles(newPatch, newOutput, brightenFactor);
     }
 
-    private void copyNewFiles(Path newPatch, Path newOutput) {
+    private void copyNewFiles(Path newPatch, Path newOutput, float brightenFactor) {
         try {
             if (Files.exists(newPatch)) {
                 Files.list(newPatch).parallel()
@@ -197,12 +207,12 @@ public class MiniTexturePacker {
                             String fileName = file.getFileName().toString();
                             if (Files.isDirectory(file)) {
                                 // if its a dir, we need to go one step deeper
-                                copyNewFiles(newPatch.resolve(file), newOutput.resolve(file));
+                                copyNewFiles(newPatch.resolve(file), newOutput.resolve(file), brightenFactor);
                             } else if (Files.isRegularFile(file)) {
                                 // if its a file we can just copy, assuming its actually a new file
                                 if (!Files.isRegularFile(newOutput.resolve(fileName))) {
                                     try {
-                                        Files.copy(newPatch.resolve(fileName), newOutput.resolve(fileName));
+                                        copyAndBrighten(newPatch.resolve(fileName), newOutput.resolve(fileName), brightenFactor);
                                     } catch (IOException e) {
                                         System.out.println("Error while coyping file " + newPatch.resolve(fileName) + " : " + e.getClass().getName() + ": " + e.getMessage());
                                     }
@@ -215,7 +225,7 @@ public class MiniTexturePacker {
         }
     }
 
-    private void copyFitting(String name, Path original, Path patch, Path output) {
+    private void copyFitting(String name, Path original, Path patch, Path output, float brightenFactor) {
         Path file = patch.resolve(name);
         if (!Files.isRegularFile(file)) {
             file = original.resolve(name);
@@ -226,7 +236,7 @@ public class MiniTexturePacker {
         }
 
         try {
-            Files.copy(file, output.resolve(name), StandardCopyOption.REPLACE_EXISTING);
+            copyAndBrighten(file, output.resolve(name), brightenFactor);
         } catch (IOException e) {
             System.err.println("Error while copying " + name + ": " + e.getClass().getName() + ": " + e.getMessage());
         }
@@ -258,5 +268,50 @@ public class MiniTexturePacker {
                 return FileVisitResult.CONTINUE;
             }
         });
+    }
+
+    private void copyAndBrighten(Path input, Path output, float factor) throws IOException {
+        if (factor != 1 && input.toString().endsWith(".png") && input.toString().contains("/blocks/")) {
+            BufferedImage in = ImageIO.read(input.toFile());
+            if (in == null) {
+                System.out.println("Couldnt brighten " + input.toString() + ", just copy OG");
+                Files.copy(input, output, StandardCopyOption.REPLACE_EXISTING);
+                return;
+            }
+            BufferedImage out = brighten(in, factor, input.toString());
+            if (out == null) {
+                return;
+            }
+            ImageIO.write(out, "png", output.toFile());
+        } else {
+            Files.copy(input, output, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    private BufferedImage brighten(BufferedImage image, float factor, String debugName) {
+        try {
+            float[] factors = new float[]{
+                    factor, factor, factor, 1f
+            };
+            float[] offsets = new float[]{
+                    0.0f, 0.0f, 0.0f, 0.0f
+            };
+            RescaleOp op = new RescaleOp(factors, offsets, null);
+            return op.filter(image, null);
+        } catch (IllegalArgumentException ex) {
+            try {
+                float[] factors = new float[]{
+                        1.4f, 1.4f, 1.4f
+                };
+                float[] offsets = new float[]{
+                        0.0f, 0.0f, 0.0f
+                };
+                RescaleOp op = new RescaleOp(factors, offsets, null);
+                return op.filter(image, null);
+            } catch (IllegalArgumentException ex2) {
+                System.out.println("Couldnt brighten " + debugName + ": " + ex2.getMessage());
+                return null;
+            }
+        }
     }
 }
